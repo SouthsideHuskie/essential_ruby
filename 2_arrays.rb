ca9687fb6f4c9a280bae6adf42c6f49348aafc90
Ryan McGuinness <ryanmmcguinness@gmail.com>
2014-04-26 12:44:25 -0500

    Homework

--- 2_arrays.rb
+++ 2_arrays.rb
@@ -4,7 +4,9 @@ students = ["Abby", "Akshat", "Alexander"]
 
 puts "The first student is #{students[0]}."
 puts "The second student is #{students[1]}."
-
+puts "The third student is #{students[2]}."
+students << "Ryan"
 students << "Andrew"
+puts "The fourth student is #{students[3]}."
 
 puts "The last student is #{students.last}."
--- challenge_1.rb
+++ challenge_1.rb
@@ -12,12 +12,24 @@
 # Before we can teach the computer how to automate a task,
 #   we usually have to do some research on how the task is done:
 #   http://en.wikipedia.org/wiki/Mortgage_calculator
+puts "I'm told you/'re looking to buy a house"
+puts "Congrat!"
+puts "By answering a few quick questions, I'll be able to estimate your monthly payment."
+puts "What is your interest rate in decimals?"
+rate=gets.to_f
+ratemon=rate/12
+puts "Great!  Now, over how many months are you financing?"
+nper=gets.to_f
+puts "Almost done.  What is the principal you are financing?"
+pv=gets.to_f
 
-def pmt(rate, nper, pv)
-  # ============================================
-  # Your code to implement the method goes here.
-  # ============================================
+def pmt(ratemon, nper, pv)
+
+(ratemon * pv * (1+ratemon)**nper)/((1+ratemon)**nper-1)
+
 end
+
+puts "Your monthly payment will be #{pmt(ratemon, nper,pv).round(2)}"
 
 # Example usage of the method:
 
